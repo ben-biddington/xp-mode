136295a20081d4b1baf3582be947bf2c96b7866c
Ben Biddington <ben.biddington@gmail.com>
2016-10-09 11:33:44 +1300

    Quieten output and forward env vars

--- test.sh
+++ test.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-sudo docker rm xp-mode-test
+sudo docker rm xp-mode-test &> /dev/null
 
 sudo docker build . -qt xp-mode && sudo docker run -itd --name xp-mode-test xp-mode
 
 sudo docker cp . xp-mode-test:/
-#sudo docker exec xp-mode-test bash -c "cat install.sh | bash"
-sudo docker exec xp-mode-test bash -c "/bin/bash ./test/checks.sh"
+sudo docker exec xp-mode-test bash -c "$@ /bin/bash ./test/checks.sh"
 
-sudo docker stop xp-mode-test
-sudo docker rm xp-mode-test
+sudo docker stop xp-mode-test &> /dev/null
+sudo docker rm xp-mode-test &> /dev/null
