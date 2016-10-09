574d3ecb0764cab1ee928153f6ebdae92ba9e6d7
Ben Biddington <ben.biddington@gmail.com>
2016-10-09 16:25:56 +1300

    It skips touching .pairs if it already exists

--- test/checks.sh
+++ test/checks.sh
@@ -3,23 +3,32 @@ filename="$HOME/.pairs"
 
 source "$(dirname $0)/support.sh"
 
-title "(1) Check that it touches .pairs when missing"
-
-pairsFileMustNotExist $filename
-
-cat install.sh | bash #&> /dev/null
-
-pairsFileMustExist $filename
-
-title "(2) The .pairs file has current git user as entry number one"
-
-clobber $filename
-
+# Before
 git config --global user.name "The Bizzz"
 git config --global user.email "the.emerald.bizz@gmail.com"
 
-cat install.sh | bash #&> /dev/null
+title "(1) Check that it touches .pairs when missing"
 
-pairsFileMustInclude $filename "The Bizzz; the.emerald.bizz@gmail.com"
+  pairsFileMustNotExist $filename
 
-pending "It skips touching .pairs if it already exists"
+  cat install.sh | bash #&> /dev/null
+
+  pairsFileMustExist $filename
+
+title "(2) The .pairs file has current git user as entry number one"
+
+  clobber $filename
+
+  cat install.sh | bash #&> /dev/null
+
+  pairsFileMustInclude $filename "The Bizzz; the.emerald.bizz@gmail.com"
+
+title "(3) It skips touching .pairs if it already exists"
+
+  clobber $filename
+
+  echo "Anyone Else; anyone.else@gmail.com" > $filename
+
+  cat install.sh | bash #&> /dev/null
+
+  pairsFileMustInclude $filename "Anyone Else; anyone.else@gmail.com"
