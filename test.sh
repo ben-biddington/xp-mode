58427a6255ef93698f2d1b2c2d2a7ba3cacf71e8
ben.biddington <ben.biddington@gmail.com>
2018-03-28 21:15:31 +1300

    Run a single test by setting the `TEST` env var

--- test.sh
+++ test.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 if [ ! -z $BUILD_SERVER ]; then
+    #todo: fix, it is missing files
     bash -c "$@ /bin/bash ./test/checks.sh"
     exit 0
 else echo "Running in container. Set \`BUILD_SERVER\` to any value to run in the current directory <`pwd`>."
@@ -11,9 +12,14 @@ sudo docker rm xp-mode-test &> /dev/null
 sudo docker build . -qt xp-mode &> /dev/null && sudo docker run -itd --name xp-mode-test xp-mode &> /dev/null
 
 sudo docker cp . xp-mode-test:/ 
-sudo docker exec xp-mode-test bash -c "$@ /bin/bash ./test/checks.sh"
-sudo docker exec xp-mode-test bash -c "$@ /bin/bash ./test/usage-checks.sh"
-sudo docker exec xp-mode-test bash -c "$@ /bin/bash ./test/select-by-name-checks.sh"
 
+if [ $TEST != "" ]; then
+    echo "Running single test file <$TEST>"
+    sudo docker exec xp-mode-test bash -c "$@ /bin/bash $TEST"
+else
+    sudo docker exec xp-mode-test bash -c "$@ /bin/bash ./test/checks.sh"
+    sudo docker exec xp-mode-test bash -c "$@ /bin/bash ./test/usage-checks.sh"
+    sudo docker exec xp-mode-test bash -c "$@ /bin/bash ./test/select-by-name-checks.sh"
+fi
 sudo docker stop xp-mode-test &> /dev/null
 sudo docker rm xp-mode-test &> /dev/null
