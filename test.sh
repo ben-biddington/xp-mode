5bf0d170f5f1905dbc02423d281cac220e4b244b
ben.biddington <ben.biddington@gmail.com>
2018-04-21 09:50:19 +1200

    Refactor: move to if else branch

--- test.sh
+++ test.sh
@@ -6,23 +6,24 @@ if [ ! -z $BUILD_SERVER ]; then
         bash -c "$@ /bin/bash $file"
     done
     exit 0
-else echo "Running in container. Set \`BUILD_SERVER\` to any value to run in the current directory <`pwd`>."
-fi
-
-sudo docker rm xp-mode-test &> /dev/null
-
-sudo docker build . -qt xp-mode &> /dev/null && sudo docker run -itd --name xp-mode-test xp-mode &> /dev/null
-
-sudo docker cp . xp-mode-test:/ 
-
-if [[ $TEST != "" ]]; then
-    echo "Running single test file <$TEST>"
-    sudo docker exec xp-mode-test bash -c "$@ /bin/bash $TEST"
 else
-    for file in ./test/*checks.sh
-    do
-        sudo docker exec xp-mode-test bash -c "$@ /bin/bash $file"
-    done
+    echo "Running in container. Set \`BUILD_SERVER\` to any value to run in the current directory <`pwd`>."
+
+    sudo docker rm xp-mode-test &> /dev/null
+
+    sudo docker build . -qt xp-mode &> /dev/null && sudo docker run -itd --name xp-mode-test xp-mode &> /dev/null
+
+    sudo docker cp . xp-mode-test:/ 
+
+    if [[ $TEST != "" ]]; then
+        echo "Running single test file <$TEST>"
+        sudo docker exec xp-mode-test bash -c "$@ /bin/bash $TEST"
+    else
+        for file in ./test/*checks.sh
+        do
+            sudo docker exec xp-mode-test bash -c "$@ /bin/bash $file"
+        done
+    fi
+    sudo docker stop xp-mode-test &> /dev/null
+    sudo docker rm xp-mode-test &> /dev/null
 fi
-sudo docker stop xp-mode-test &> /dev/null
-sudo docker rm xp-mode-test &> /dev/null
